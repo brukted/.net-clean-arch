53617f508955d9197a2da8ce3f0be59705717221
Bruk Tedla <bruk@kemercode.com>
2023-04-25 09:30:12 +0300

    LeaveAllocation CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Requests/Queries/GetLeaveAllocationListRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Requests.Queries
+{
+    public class GetLeaveAllocationListRequest : IRequest<List<LeaveAllocationDto>>
+    {
+    }
+}
+
