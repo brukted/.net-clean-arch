2697a0dd3b850d3b51f19ea5c60449e05c66bdab
Bruk Tedla <bruk@kemercode.com>
2023-04-28 09:49:18 +0300

    add controllers

--- HR.LeaveManagement.Application/Features/LeaveTypes/Requests/Queries/GetLeaveTypeDetailRequest.cs
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Requests/Queries/GetLeaveTypeDetailRequest.cs
@@ -2,7 +2,7 @@
 using HR.LeaveManagement.Application.DTOs;
 using MediatR;
 
-namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries
 {
     public class GetLeaveTypeDetailRequest : IRequest<LeaveTypeDto>
     {
